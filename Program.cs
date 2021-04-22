c6dfcdd55d47ae286eadafcd2c2e2e1fd89184a5
Chroma-Z <55671546+Chroma-Z@users.noreply.github.com>
2021-04-22 14:47:16 -0500

    Change the Hello World Message

--- Program.cs
+++ Program.cs
@@ -6,7 +6,8 @@ namespace Game_Git_Demo
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello version control! : )");
+            Console.ReadKey(true);
         }
     }
 }
