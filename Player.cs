53b6da3249439d2fff80943041cb2aee37dfa733
Chroma-Z <55671546+Chroma-Z@users.noreply.github.com>
2021-04-22 15:00:08 -0500

    Created Player Class

---
+++ Player.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Game_Git_Demo
+{
+    class Player
+    {
+        public string Name { get; private set; }
+
+        public Player(string name)
+        {
+            Name = name;
+        }
+    }
+}
